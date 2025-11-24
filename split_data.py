c8a4fd350cab5e3dde28b0abbd9ed3336d6514f9
Carolina Branas <carobrasor@gmail.com>
2025-11-24 14:02:54 +0100

    Add final updates

--- split_data.py
+++ split_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('data/classified_missions_gpt4omini.csv')
+df = pd.read_csv('data/classified_missions_gpt4omini_PROMPT2.csv')
 
 train_df, test_df = train_test_split(
     df,
@@ -10,8 +10,8 @@ train_df, test_df = train_test_split(
     random_state=42
 )
 
-train_df.to_csv("train_test_datasets/train.csv", index=False)
-test_df.to_csv("train_test_datasets/test.csv", index=False)
+train_df.to_csv("train_test_datasets/train_PROMPT2.csv", index=False)
+test_df.to_csv("train_test_datasets/test_PROMPT2.csv", index=False)
 
 majority = train_df[train_df.label == 0]
 minority = train_df[train_df.label == 1]
@@ -26,4 +26,4 @@ minority_upsampled = minority.sample(
 train_balanced = pd.concat([majority, minority_upsampled], ignore_index=True)
 train_balanced = train_balanced.sample(frac=1, random_state=42)  # shuffle
 
-train_balanced.to_csv("train_balanced.csv", index=False)
+train_balanced.to_csv("train_test_datasets/train_balanced_PROMPT2.csv", index=False)
